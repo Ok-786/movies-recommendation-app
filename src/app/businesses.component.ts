eab7824a9609379375495ac43f46637225f38943
Ok-786 <osamakiyani6123@gmail.com>
2023-12-02 14:24:44 +0500

    updated bunissess page

--- src/app/businesses.component.ts
+++ src/app/businesses.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WebService } from './web.service';
-
 
 @Component({
   selector: 'businesses',
   templateUrl: './businesses.component.html',
   styleUrls: ['./businesses.component.css']
 })
-export class BusinessesComponent {
+export class BusinessesComponent implements OnInit {
 
     business_list: any = [];
     page: number = 1;
@@ -20,24 +19,22 @@ export class BusinessesComponent {
       }
 
       this.business_list = this.webService.getBusinesses(this.page);
+
+      // Log business_list to the console
+      console.log('Business List:', this.business_list);
     }
 
     previousPage() {
       if (this.page > 1) {
         this.page = this.page - 1;
         sessionStorage['page'] = this.page;
-         this.business_list =
-         this.webService.getBusinesses(this.page);
-        
-        }
-     }
+        this.business_list = this.webService.getBusinesses(this.page);
+      }
+    }
 
     nextPage() {
       this.page = this.page + 1;
       sessionStorage['page'] = this.page;
-      this.business_list =
-      this.webService.getBusinesses(this.page);
-     }
-
-
+      this.business_list = this.webService.getBusinesses(this.page);
+    }
 }
